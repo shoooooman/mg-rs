ebe57b7a0aeb31cd604c62fbe75a83967bc7a788
shoooooman <shoma216@gmail.com>
2020-11-11 16:36:39 +0900

    add top mechanism test

---
+++ market/top_test.go
@@ -0,0 +1,35 @@
+package market
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/shoooooman/mg-rs/common"
+)
+
+func TestTopGateway_GetTxReq(t *testing.T) {
+	tg := &TopGateway{}
+
+	ratings := map[int]float64{
+		0: 1.0,
+		1: 3.0,
+		2: -2.0,
+		4: 0.0,
+	}
+	expected := []common.TxReq{
+		{
+			ID:    0,
+			Party: 1,
+		},
+		{
+			ID:    1,
+			Party: 1,
+		},
+	}
+	for i := 0; i < len(expected); i++ {
+		req := tg.GetTxReq(ratings)
+		if !reflect.DeepEqual(req, expected[i]) {
+			t.Errorf("\nexpected: %v\nactual: %v\n", expected[i], req)
+		}
+	}
+}
