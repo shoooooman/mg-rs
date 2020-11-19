7191452c5948468c392e859bce34095a05676df0
shoooooman <shoma216@gmail.com>
2020-11-19 18:11:39 +0900

    add kill cmd to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
+if [ $1 = "start" ]; then
+    go build main.go
 
-num=5
-for (( i=0; i < $num; i++ )); do
-    go run main.go $i &
-done
+    num=50
+    for (( i=0; i < $num; i++ )); do
+        ./main $i --tag=mg-rs &
+    done
+    rm ./main
+elif [ $1 = "kill" ]; then
+    pgrep -f mg-rs | xargs kill
+else
+    echo please input valid command
+fi
